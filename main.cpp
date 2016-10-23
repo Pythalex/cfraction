497dfbb0a85036cb2e831f55a0393bc3cac16e6f
Pythalex <alexboncorot@yahoo.fr>
2016-10-23 19:08:37 +0200

    Operations added
    Added a method to check sign of a cfraction and modify if need (e.g. a/-b
    become -a/b )
    
    Classic and shortcut operations + - / * between cfraction added
    
    Started + operation between cfraction and int : problem when addition
    concerns a negative int, to fix

--- main.cpp
+++ main.cpp
@@ -4,23 +4,58 @@
 #include "cfraction.h"
 
 void cfraction_test() {
-	cfraction fraction1(8,2);
+	cfraction fraction1(8,-1);
 	cfraction fraction2(4,8);
 	cfraction fraction3(1,2);
 
-	std::cout << fraction2 + fraction3 << std::endl;
-	std::cout << fraction1 - fraction2 - fraction3 << std::endl;
-	std::cout << fraction2 * fraction3 << std::endl;
-	std::cout << fraction1 / fraction3 << std::endl;
+	// Afficher fractions
+	std::cout << fraction1 << std::endl;
+	std::cout << fraction2 << std::endl;
+	std::cout << fraction3 << std::endl << std::endl;
+
+	// Operateurs arithmétiques classiques
+	std::cout << "Operateurs arithmetiques classiques" << std::endl;
+	std::cout << fraction2 << " + " << fraction3 << " = " << fraction2 + fraction3 << std::endl;
+	std::cout << fraction1 << " - " << fraction2 << " - " << fraction3 << " = " << fraction1 - fraction2 - fraction3 << std::endl;
+	std::cout << fraction2  << " * " << fraction3 << " = " << fraction2 * fraction3 << std::endl;
+	std::cout << fraction1  << " / " << fraction3 << " = " << fraction1 / fraction3 << std::endl << std::endl;
+	std::cout << std::endl;
+	std::cout << "(avec des int)" << std::endl;
+	std::cout << fraction1 << " + " << 5 << " = " << fraction1 + 5 << std::endl;
+	std::cout << fraction3 << " + " << -2 << " = " << fraction1 + (-2) << std::endl;
+	std::cout << std::endl;
+
+	// Operateurs arithmétiques raccourcis
+	std::cout << "Operateurs arithmetiques raccourcis" << std::endl;
+	std::cout << fraction1 << " += " << fraction2 << " --> ";
+	fraction1 += fraction2;
+	std::cout << fraction1 << std::endl;
+	std::cout << fraction1 << " -= " << fraction2 << " --> ";
+	fraction1 -= fraction2;
+	std::cout << fraction1 << std::endl;
+	std::cout << fraction1 << " *= " << fraction2 << " --> ";
+	fraction1 *= fraction2;
+	std::cout << fraction1 << std::endl;
+	std::cout << fraction1 << " /= " << fraction2 << " --> ";
+	fraction1 /= fraction2;
+	std::cout << fraction1 << std::endl << std::endl;
+
+	// Operateurs de comparaison
+	std::cout << "Operateurs de comparaison" << std::endl;
+	std::cout << fraction2 << " == " << fraction3 << " = " << (fraction2 == fraction3 ? "true" : "false") << std::endl;
+	std::cout << fraction1 << " < " << fraction3 << " = " << (fraction1 < fraction3 ? "true" : "false") << std::endl;
+	std::cout << fraction1 << " > " << fraction3 << " = " << (fraction1 > fraction3 ? "true" : "false") << std::endl;
 }
 
 
 int main() {
 
 	/// FOR DEBUG BEGIN
-	std::cout << "=== DEBUG PROGRAM ===\n";
+	std::cout << "=== DEBUG PROGRAM ===\n" << std::endl;
 	// Tests
 	cfraction_test();
+	std::cout << std::endl;
+	std::cout << "===      END      ===\n";
 	/// FOR DEBUG END
 
 	// Anti-exit
